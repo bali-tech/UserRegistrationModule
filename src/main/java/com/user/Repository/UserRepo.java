1fa103e9f8326938efa3438a2f33da4e8e2803b4
bali-tech <baliramkale12@gmail.com>
2022-09-14 10:19:20 +0530

    second commit

--- src/main/java/com/user/Repository/UserRepo.java
+++ src/main/java/com/user/Repository/UserRepo.java
@@ -2,13 +2,13 @@ package com.user.Repository;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.user.Model.UserReg;
 
 @Repository
+
 public interface UserRepo extends JpaRepository<UserReg, String> {
 	List<UserReg> findByFirstName(String firstName);
 
